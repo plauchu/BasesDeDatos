a5929bc57a87d858b7f39c8242b0132bf2d6a5f1
Rodrigo Plauchú Rodríguez <rplauchur@gmail.com>
2022-06-21 23:54:24 -0500

    Add files via upload

---
+++ tarea6A/tarea6A/Menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tarea6A
+{
+  public partial class Menu : Form
+  {
+    //Variables de clase
+    PréstamosEntities context = new PréstamosEntities();
+    string rfc;
+    public Menu()
+    {
+      InitializeComponent();
+    }
+
+    public Menu(PréstamosEntities context, string rfc)
+    {
+      InitializeComponent();
+      this.context = context;
+      this.rfc = rfc;
+      if(rfc == "ger")
+      {
+        menuActTablas.Enabled = true;
+        menuActPrest.Enabled = true;
+      }
+      else
+      {
+        menuVer.Enabled = true;
+      }
+    }
+
+
+
+    private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    private void menuActTablas_Click(object sender, EventArgs e)
+    {
+      actualizaTablas a = new actualizaTablas();
+      a.ShowDialog();
+
+    }
+
+    private void menuActPrest_Click(object sender, EventArgs e)
+    {
+      actPrest a = new actPrest();
+      a.ShowDialog();
+    }
+
+    private void menuVer_Click(object sender, EventArgs e)
+    {
+      verCliente a = new verCliente(rfc);
+      a.ShowDialog();
+
+    }
+  }
+}
---
+++ tarea6A/tarea6A/actualizaTablas.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace tarea6A
+{
+  public partial class actualizaTablas : Form
+  {
+    //Variables de clase
+    PréstamosEntities context = new PréstamosEntities();
+    BindingSource AvalesBS = new BindingSource();
+    BindingSource ClientesBS = new BindingSource();
+    BindingSource EmpresasBS = new BindingSource();
+
+    public actualizaTablas()
+    {
+      InitializeComponent();
+    }
+
+        private void actualizaTablas_Load(object sender, EventArgs e)
+        {
+      context.Avales.Load();
+      context.Clientes.Load();
+      context.Empresas.Load();
+
+
+
+        }
+
+    private void cbTablas_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      if (cbTablas.Text == "Avales")
+      {
+        AvalesBS.DataSource = context.Avales.Local;
+        dtgGeneral.DataSource = AvalesBS;
+        bindingNavigator.BindingSource = AvalesBS;
+
+      }
+      else if (cbTablas.Text == "Clientes")
+      {
+        ClientesBS.DataSource = context.Clientes.Local;
+        dtgGeneral.DataSource = ClientesBS;
+        bindingNavigator.BindingSource = ClientesBS;
+
+      }
+      else
+      {
+        EmpresasBS.DataSource = context.Empresas.Local;
+        dtgGeneral.DataSource = EmpresasBS;
+        bindingNavigator.BindingSource = EmpresasBS;
+      }
+
+    }
+
+    private void btAlta_Click(object sender, EventArgs e)
+    {
+      try
+      {
+
+        context.SaveChanges();
+      }catch(Exception ex)
+      {
+        MessageBox.Show("No se pudieron guardar los cambios. Asegúrate de que la información es correcta. O si borraste algo que no deseabas, regresa al menú.");
+      }
+    }
+
+    private void dtgGeneral_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+
+    }
+
+    private void dtgGeneral_DataError(object sender, DataGridViewDataErrorEventArgs e)
+    {
+      MessageBox.Show("Hay un error en los datos que ingresaste. Asegúrate que sean del tipo correcto.");
+    }
+
+    private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
